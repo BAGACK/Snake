33be4c65e15a74a3df4ed57cda5c4c44eda5fe97
mepeisen <mep_eisen@web.de>
2016-06-28 10:42:15 +0200

    fixed sheep yaw

--- Snake-1.10/src/main/java/com/comze_instancelabs/mgsnake/nms/MEFallingBlock1_10.java
+++ Snake-1.10/src/main/java/com/comze_instancelabs/mgsnake/nms/MEFallingBlock1_10.java
@@ -25,10 +25,10 @@ public class MEFallingBlock1_10 extends EntityFallingBlock implements FallingBlo
 		return Block.getByCombinedId(35 + (color << 12));
 	}
 
-	public void setYaw(Location target) {
-		final Location loc = new Location(target.getWorld(), this.locX, this.locY, this.locZ);
-		loc.setDirection(target.toVector().subtract(loc.toVector()));
-		this.yaw = loc.getYaw();
+	public void setYaw(Location l2) {
+//		final Location l1 = this.getBukkitEntity().getLocation();
+//		l1.setDirection(l1.toVector().subtract(l2.toVector()));
+		this.yaw = l2.getYaw();
 	}
 
 	@Override
